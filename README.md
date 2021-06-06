76a21232a7e12c470f7ea4199a18a5a5d87d897a
Darko Trifunovski <dtrifuno@gmail.com>
2021-06-06 07:50:08 +0200

    Solve 4.1 (parity)

---
+++ README.md
@@ -0,0 +1,22 @@
+# Rust Solutions to _Elements of Programming Interviews in Java_
+
+These are my solutions in Rust for the exercises in the book [Elements of Programming Interviews in Java: The Insiders' Guide](http://elementsofprogramminginterviews.com/)
+by _Adnan Aziz_, _Tsung-Hsien Lee_ and _Amit Prakash_.
+
+All solutions are tested using parameterized testing with the [rstest](https://crates.io/crates/rstest/0.2.2) crate.
+
+## Usage
+
+To download the package and execute all tests, run
+
+```
+git clone https://github.com/dtrifuno/epi-rust.git
+cd epi-rust
+cargo test
+```
+
+or, `cargo test $NAME` to run only those tests that have `$NAME` as part of their fully qualified name.
+
+## License
+
+[MIT](https://choosealicense.com/licenses/mit/)
---
+++ src/chapter04/parity.rs
@@ -0,0 +1,46 @@
+const PRECOMPUTED_PARITY_SIZE: usize = u16::MAX as usize;
+const BIT_MASK: usize = 0xFFFF;
+
+pub struct ParityCalculator {
+    precomputed_parity: [u8; PRECOMPUTED_PARITY_SIZE],
+}
+
+impl ParityCalculator {
+    pub fn new() -> ParityCalculator {
+        let mut precomputed_parity = [0; PRECOMPUTED_PARITY_SIZE];
+
+        for i in 0..PRECOMPUTED_PARITY_SIZE {
+            let mut j = i;
+            let mut parity = 0;
+            while j != 0 {
+                parity = parity ^ 1;
+                j = j & (j - 1);
+            }
+            precomputed_parity[i] = parity;
+        }
+        return ParityCalculator { precomputed_parity };
+    }
+
+    pub fn calculate_parity(&self, n: u64) -> u8 {
+        self.precomputed_parity[(n & BIT_MASK as u64) as usize]
+            ^ self.precomputed_parity[(n >> 16 & BIT_MASK as u64) as usize]
+            ^ self.precomputed_parity[(n >> 32 & BIT_MASK as u64) as usize]
+            ^ self.precomputed_parity[(n >> 48 & BIT_MASK as u64) as usize]
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use rstest::*;
+
+    #[rstest]
+    #[case(0, 0)]
+    #[case(1, 1)]
+    #[case(0b_0100_1000_0010, 1)]
+    #[case(0b_0100_1000_0010_1110, 0)]
+    fn calculate_parity(#[case] input: u64, #[case] expected: u8) {
+        let pc = ParityCalculator::new();
+        assert_eq!(expected, pc.calculate_parity(input));
+    }
+}
